07c74bab0a8232d1d41dfcd4bf7a1a41951c100f
Andrea Fornaia <fornaia@dmi.unict.it>
2022-06-17 15:39:52 +0200

    Fix: rimuove stampa hard coded del groupId

--- src/main/java/com/example/demo/KafkaConsumerService.java
+++ src/main/java/com/example/demo/KafkaConsumerService.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Header;
@@ -14,11 +15,14 @@ import org.springframework.kafka.support.KafkaHeaders;
 public class KafkaConsumerService {
 	private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerService.class);
 
+	@Value("${group.id}")
+	private String groupId;
+
 	@KafkaListener(topics = "${topic.name}", groupId = "${group.id}")
 	public void listenWithHeaders(
 			@Payload String message,
 			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
-		logger.info("Received Message in group foo: " + message + " from partition: " + partition);
+		logger.info("Received Message in group " + groupId + ": " + message + " from partition: " + partition);
 	}
 
 }
